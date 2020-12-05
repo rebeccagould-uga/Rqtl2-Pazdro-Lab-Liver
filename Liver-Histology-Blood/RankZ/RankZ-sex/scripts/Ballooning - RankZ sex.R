8e554dd3236c78d65b97a577d5b2efa2818a1a36
rebeccagould-uga <rebecca.gould@uga.edu>
2020-12-04 20:17:39 -0500

    Updated histology-sex results

--- Liver-Histology-Blood/RankZ/RankZ-sex/scripts/Ballooning - RankZ sex.R
+++ Liver-Histology-Blood/RankZ/RankZ-sex/scripts/Ballooning - RankZ sex.R
@@ -9,7 +9,7 @@
 
 
 
-setwd("/users/becca/R01_Ballooning_DO_mapping_Liver/data")
+setwd("/users/becca/R01_GSH_DO_mapping_Liver/data")
 
 library(qtl2)
 library (tidyverse)
@@ -37,17 +37,17 @@ pdf(file = "Ballooning QTL Results - RankZ sex.pdf")
 
   par(mar=c(4.1, 4.1, 2.6, 2.6))
   threshold_Ballooning = summary(perm_Ballooning, alpha = c(0.2, 0.1, 0.05))
-  plot_scan1(x = qtlscan_Ballooning, map = R01_Ballooning_DO_QTLdata$gmap,  main = "Genome Scan for Ballooning", ylim = c(0,11))
+  plot_scan1(x = qtlscan_Ballooning, map = R01_GSH_DO_QTLdata$gmap,  main = "Genome Scan for Ballooning", ylim = c(0,11))
   abline(h = threshold_Ballooning, col = c("purple", "red", "blue"), lwd = 2)
 
 #using gmap (cM)
-  find_peaks(scan1_output = qtlscan_Ballooning, map = R01_Ballooning_DO_QTLdata$gmap, threshold = summary(perm_Ballooning, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
-  gmap_peaksBallooning <- find_peaks(scan1_output = qtlscan_Ballooning, map = R01_Ballooning_DO_QTLdata$gmap, threshold = summary(perm_Ballooning, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
+  find_peaks(scan1_output = qtlscan_Ballooning, map = R01_GSH_DO_QTLdata$gmap, threshold = summary(perm_Ballooning, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
+  gmap_peaksBallooning <- find_peaks(scan1_output = qtlscan_Ballooning, map = R01_GSH_DO_QTLdata$gmap, threshold = summary(perm_Ballooning, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
   print(gmap_peaksBallooning)
   
 #using pmap (Mbp)
-  find_peaks(scan1_output = qtlscan_Ballooning, map = R01_Ballooning_DO_QTLdata$pmap, threshold = summary(perm_Ballooning, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
-  peaksBallooning <- find_peaks(scan1_output = qtlscan_Ballooning, map = R01_Ballooning_DO_QTLdata$pmap, threshold = summary(perm_Ballooning, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
+  find_peaks(scan1_output = qtlscan_Ballooning, map = R01_GSH_DO_QTLdata$pmap, threshold = summary(perm_Ballooning, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
+  peaksBallooning <- find_peaks(scan1_output = qtlscan_Ballooning, map = R01_GSH_DO_QTLdata$pmap, threshold = summary(perm_Ballooning, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
   print(peaksBallooning)
   
 
@@ -60,32 +60,8 @@ write_xlsx(list("Ballooning gmap (cM)" = gmap_peaksBallooning,
 ## Estimate QTL Effects (Coefficients) + Connect to SNP and Gene Databases
 ####################################################
 
-#For Ballooning --- Chromosome 2
-  par(mar=c(4.1, 4.1, 2.6, 2.6))
+#no chromosomes of interest 
 
-  #using gmap (cM)
-  chr = 2
-  coef_blup_Ballooning_chr2 <- scan1blup(genoprobs =  probs[,chr], pheno = pheno["zBallooning"], kinship = kinship_loco[[chr]], addcovar = sex, cores = 10)
-  plot_coefCC(x = coef_blup_Ballooning_chr2, map = R01_Ballooning_DO_QTLdata$gmap, scan1_output = qtlscan_Ballooning, main = "Ballooning BLUPs plotted with CC Founders", legend = "bottomleft", bgcolor="gray95")
-  xlim <- c(45,65)
-  plot_coefCC(x = coef_blup_Ballooning_chr2, map = R01_Ballooning_DO_QTLdata$gmap, scan1_output = qtlscan_Ballooning, main = "Ballooning BLUPs plotted with CC Founders", legend = "bottomleft", bgcolor="gray95", xlim = xlim)
-  
-  #using pmap (Mbp)
-  chr = 2
-  #could use ci_lo or ci_hi, but for this case, I want a specific chromosome 2 peak
-  #start = peaksBallooning[peaksBallooning$chr ==  chr,"ci_lo"]
-  #end = peaksBallooning[peaksBallooning$chr == chr, "ci_hi"] 
-  
-  pander(peaksBallooning)
-  #based on peaksBallooning, peak of interest is ~109 Mbp
-  variants_Ballooning_chr2 <- query_variants(chr, 107, 111)
-  out_snps_Ballooning_chr2 <- scan1snps(genoprobs = probs, map = R01_Ballooning_DO_QTLdata$pmap, pheno = pheno["zBallooning"], kinship = kinship_loco[[chr]], addcovar = sex, query_func = query_variants,
-                                      chr = chr, start = 107, end = 111, keep_all_snps = TRUE)
-  plot_snpasso(out_snps_Ballooning_chr2$lod, out_snps_Ballooning_chr2$snpinfo, main = "Ballooning SNPs")
-  
-  Ballooning_Genes_MGI_chr2 <- query_genes_mgi(chr = chr, start = 107, end = 111)
-  plot(out_snps_Ballooning_chr2$lod, out_snps_Ballooning_chr2$snpinfo, drop_hilit=1.5, genes = Ballooning_Genes_MGI_chr2, main = "Ballooning Genes MGI")
-  
 dev.off()
   
   
@@ -95,7 +71,7 @@ dev.off()
 ####################################################
 
 pdf(file = "Ballooning GWAS - RankZ sex.pdf")
-out_gwas_Ballooning <- scan1snps(genoprobs = probs, map = R01_Ballooning_DO_QTLdata$pmap, pheno = pheno["zBallooning"], kinship = kinship_loco, addcovar = sex, query_func=query_variants, cores=10)
+out_gwas_Ballooning <- scan1snps(genoprobs = probs, map = R01_GSH_DO_QTLdata$pmap, pheno = pheno["zBallooning"], kinship = kinship_loco, addcovar = sex, query_func=query_variants, cores=10)
 par(mar=c(4.1, 4.1, 2.6, 2.6))
 plot(out_gwas_Ballooning$lod, out_gwas_Ballooning$snpinfo, altcol="green4", gap=0, main = "Ballooning GWAS", ylim = c(0,6))
 dev.off()
@@ -104,6 +80,6 @@ dev.off()
 ## Heritability calculation - the ratio of genetic variance to total variance using a linear mixed model
 ####################################################
 
-herit_Ballooning_sex <- est_herit(pheno["zBallooning"], kinship_lmm, sex, cores = 10)
-herit_Ballooning_sexgen <- est_herit(pheno["zBallooning"], kinship_lmm, sexgen, cores = 10)
+herit_Ballooning_sex <- est_herit(pheno["zBallooning"], kinship_lmm, sex, cores = 2)
+herit_Ballooning_sexgen <- est_herit(pheno["zBallooning"], kinship_lmm, sexgen, cores = 2)
 
--- Liver-Histology-Blood/RankZ/RankZ-sex/scripts/Steatosis - RankZ sex.R
+++ Liver-Histology-Blood/RankZ/RankZ-sex/scripts/Steatosis - RankZ sex.R
@@ -8,7 +8,7 @@
 #Run RankZ Transformation and Data Prep R Script before doing this**
 
 
-setwd("/users/becca/R01_Steatosis_DO_mapping_Liver/data")
+setwd("/users/becca/R01_GSH_DO_mapping_Liver/data")
 
 library(qtl2)
 library (tidyverse)
@@ -36,17 +36,17 @@ pdf(file = "Steatosis QTL Results - RankZ sex.pdf")
 
   par(mar=c(4.1, 4.1, 2.6, 2.6))
   threshold_Steatosis = summary(perm_Steatosis, alpha = c(0.2, 0.1, 0.05))
-  plot_scan1(x = qtlscan_Steatosis, map = R01_Steatosis_DO_QTLdata$gmap,  main = "Genome Scan for Steatosis", ylim = c(0,11))
+  plot_scan1(x = qtlscan_Steatosis, map = R01_GSH_DO_QTLdata$gmap,  main = "Genome Scan for Steatosis", ylim = c(0,11))
   abline(h = threshold_Steatosis, col = c("purple", "red", "blue"), lwd = 2)
 
 #using gmap (cM)
-  find_peaks(scan1_output = qtlscan_Steatosis, map = R01_Steatosis_DO_QTLdata$gmap, threshold = summary(perm_Steatosis, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
-  gmap_peaksSteatosis <- find_peaks(scan1_output = qtlscan_Steatosis, map = R01_Steatosis_DO_QTLdata$gmap, threshold = summary(perm_Steatosis, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
+  find_peaks(scan1_output = qtlscan_Steatosis, map = R01_GSH_DO_QTLdata$gmap, threshold = summary(perm_Steatosis, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
+  gmap_peaksSteatosis <- find_peaks(scan1_output = qtlscan_Steatosis, map = R01_GSH_DO_QTLdata$gmap, threshold = summary(perm_Steatosis, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
   print(gmap_peaksSteatosis)
   
 #using pmap (Mbp)
-  find_peaks(scan1_output = qtlscan_Steatosis, map = R01_Steatosis_DO_QTLdata$pmap, threshold = summary(perm_Steatosis, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
-  peaksSteatosis <- find_peaks(scan1_output = qtlscan_Steatosis, map = R01_Steatosis_DO_QTLdata$pmap, threshold = summary(perm_Steatosis, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
+  find_peaks(scan1_output = qtlscan_Steatosis, map = R01_GSH_DO_QTLdata$pmap, threshold = summary(perm_Steatosis, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
+  peaksSteatosis <- find_peaks(scan1_output = qtlscan_Steatosis, map = R01_GSH_DO_QTLdata$pmap, threshold = summary(perm_Steatosis, alpha = 0.2), peakdrop = 1.8, drop = 1.5, expand2markers = FALSE)
   print(peaksSteatosis)
   
 
@@ -59,32 +59,19 @@ write_xlsx(list("Steatosis gmap (cM)" = gmap_peaksSteatosis,
 ## Estimate QTL Effects (Coefficients) + Connect to SNP and Gene Databases
 ####################################################
 
-#For Steatosis --- Chromosome 2
+#For Steatosis --- Chromosome 18
   par(mar=c(4.1, 4.1, 2.6, 2.6))
 
   #using gmap (cM)
-  chr = 2
-  coef_blup_Steatosis_chr2 <- scan1blup(genoprobs =  probs[,chr], pheno = pheno["zSteatosis"], kinship = kinship_loco[[chr]], addcovar = sex, cores = 10)
-  plot_coefCC(x = coef_blup_Steatosis_chr2, map = R01_Steatosis_DO_QTLdata$gmap, scan1_output = qtlscan_Steatosis, main = "Steatosis BLUPs plotted with CC Founders", legend = "bottomleft", bgcolor="gray95")
-  xlim <- c(45,65)
-  plot_coefCC(x = coef_blup_Steatosis_chr2, map = R01_Steatosis_DO_QTLdata$gmap, scan1_output = qtlscan_Steatosis, main = "Steatosis BLUPs plotted with CC Founders", legend = "bottomleft", bgcolor="gray95", xlim = xlim)
+  chr = 18
+  coef_blup_Steatosis_chr18 <- scan1blup(genoprobs =  probs[,chr], pheno = pheno["zSteatosis"], kinship = kinship_loco[[chr]], addcovar = sex, cores = 10)
+  plot_coefCC(x = coef_blup_Steatosis_chr18, map = R01_GSH_DO_QTLdata$gmap, scan1_output = qtlscan_Steatosis, main = "Steatosis BLUPs plotted with CC Founders", legend = "bottomleft", bgcolor="gray95")
+  xlim <- c(15,40)
+  plot_coefCC(x = coef_blup_Steatosis_chr18, map = R01_GSH_DO_QTLdata$gmap, scan1_output = qtlscan_Steatosis, main = "Steatosis BLUPs plotted with CC Founders", legend = "bottomleft", bgcolor="gray95", xlim = xlim)
+  xlim <- c(35,60)
+  plot_coefCC(x = coef_blup_Steatosis_chr18, map = R01_GSH_DO_QTLdata$gmap, scan1_output = qtlscan_Steatosis, main = "Steatosis BLUPs plotted with CC Founders", legend = "bottomleft", bgcolor="gray95", xlim = xlim)
   
-  #using pmap (Mbp)
-  chr = 2
-  #could use ci_lo or ci_hi, but for this case, I want a specific chromosome 2 peak
-  #start = peaksSteatosis[peaksSteatosis$chr ==  chr,"ci_lo"]
-  #end = peaksSteatosis[peaksSteatosis$chr == chr, "ci_hi"] 
-  
-  pander(peaksSteatosis)
-  #based on peaksSteatosis, peak of interest is ~109 Mbp
-  variants_Steatosis_chr2 <- query_variants(chr, 107, 111)
-  out_snps_Steatosis_chr2 <- scan1snps(genoprobs = probs, map = R01_Steatosis_DO_QTLdata$pmap, pheno = pheno["zSteatosis"], kinship = kinship_loco[[chr]], addcovar = sex, query_func = query_variants,
-                                      chr = chr, start = 107, end = 111, keep_all_snps = TRUE)
-  plot_snpasso(out_snps_Steatosis_chr2$lod, out_snps_Steatosis_chr2$snpinfo, main = "Steatosis SNPs")
-  
-  Steatosis_Genes_MGI_chr2 <- query_genes_mgi(chr = chr, start = 107, end = 111)
-  plot(out_snps_Steatosis_chr2$lod, out_snps_Steatosis_chr2$snpinfo, drop_hilit=1.5, genes = Steatosis_Genes_MGI_chr2, main = "Steatosis Genes MGI")
-  
+ 
 dev.off()
   
   
@@ -94,7 +81,7 @@ dev.off()
 ####################################################
 
 pdf(file = "Steatosis GWAS - RankZ sex.pdf")
-out_gwas_Steatosis <- scan1snps(genoprobs = probs, map = R01_Steatosis_DO_QTLdata$pmap, pheno = pheno["zSteatosis"], kinship = kinship_loco, addcovar = sex, query_func=query_variants, cores=10)
+out_gwas_Steatosis <- scan1snps(genoprobs = probs, map = R01_GSH_DO_QTLdata$pmap, pheno = pheno["zSteatosis"], kinship = kinship_loco, addcovar = sex, query_func=query_variants, cores=10)
 par(mar=c(4.1, 4.1, 2.6, 2.6))
 plot(out_gwas_Steatosis$lod, out_gwas_Steatosis$snpinfo, altcol="green4", gap=0, main = "Steatosis GWAS", ylim = c(0,6))
 dev.off()
@@ -103,6 +90,6 @@ dev.off()
 ## Heritability calculation - the ratio of genetic variance to total variance using a linear mixed model
 ####################################################
 
-herit_Steatosis_sex <- est_herit(pheno["zSteatosis"], kinship_lmm, sex, cores = 10)
-herit_Steatosis_sexgen <- est_herit(pheno["zSteatosis"], kinship_lmm, sexgen, cores = 10)
+herit_Steatosis_sex <- est_herit(pheno["zSteatosis"], kinship_lmm, sex, cores = 2)
+herit_Steatosis_sexgen <- est_herit(pheno["zSteatosis"], kinship_lmm, sexgen, cores = 2)
 
